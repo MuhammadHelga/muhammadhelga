2edd06f40bdbff44ffbac26f6d32397b80e3415c
Muhammad Helga <muhammadhelga41@gmail.com>
2025-01-20 22:39:02 +0700

    new update 4.4

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import '../styles/Experience.css'
+import icondsi from '../assets/dsi.png'
 
 import React from 'react'
 
@@ -13,7 +14,8 @@ function Experience() {
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
                 date="Oct 2023 - Nov 2023"
-                iconStyle={{ background: 'rgb(70, 78, 85)', color: '#fff' }}
+                iconStyle={{ background: 'rgb(70, 78, 85)', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                icon={<img src={icondsi} alt='dsi icon' style={{ width: '90%', height: '90%', borderRadius: '50%', objectFit: 'cover' }}/>}
             >
                 <h3 className="vertical-timeline-element-title">Staff Event Division</h3>
                 <h4 className="vertical-timeline-element-subtitle">LKMM Information Systems Department</h4>

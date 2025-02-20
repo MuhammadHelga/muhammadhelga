6cda7622e29f40e36e0e462b1e446cc31ca091c0
Muhammad Helga <muhammadhelga41@gmail.com>
2025-02-20 20:22:39 +0700

    updateExperience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,6 +2,7 @@ import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timel
 import 'react-vertical-timeline-component/style.min.css';
 import '../styles/Experience.css'
 import icondsi from '../assets/dsi.png'
+import iconFilkom from '../assets/filkom.png'
 
 import React from 'react'
 
@@ -23,7 +24,43 @@ function Experience() {
                 Become staff in the events division to run the LKMM series of the Information Systems Department
                 </p>
             </VerticalTimelineElement>
-        </VerticalTimeline>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="Feb 2025 - Present"
+                iconStyle={{ background: 'rgb(70, 78, 85)', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                icon={<img src={iconFilkom} alt='dsi icon' style={{ width: '90%', height: '90%', borderRadius: '50%', objectFit: 'cover' }}/>}
+                >
+                <h3 className="vertical-timeline-element-title">Mobile Application Development Laboratory Practicum Assistant</h3>
+                <h4 className="vertical-timeline-element-subtitle">Faculty of Computer Science (FILKOM) University of Brawijaya</h4>
+                <p>
+                
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="Feb 2025 - Present"
+                iconStyle={{ background: 'rgb(70, 78, 85)', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                icon={<img src={iconFilkom} alt='dsi icon' style={{ width: '90%', height: '90%', borderRadius: '50%', objectFit: 'cover' }}/>}
+                >
+                <h3 className="vertical-timeline-element-title">System Administration Laboratory Practicum Assistant</h3>
+                <h4 className="vertical-timeline-element-subtitle">Faculty of Computer Science (FILKOM) University of Brawijaya</h4>
+                <p>
+                
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="Feb 2025 - Present"
+                iconStyle={{ background: 'rgb(70, 78, 85)', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                icon={<img src={iconFilkom} alt='dsi icon' style={{ width: '90%', height: '90%', borderRadius: '50%', objectFit: 'cover' }}/>}
+                >
+                <h3 className="vertical-timeline-element-title">System Integration Technology Laboratory Practicum Assistant</h3>
+                <h4 className="vertical-timeline-element-subtitle">Faculty of Computer Science (FILKOM) University of Brawijaya</h4>
+                <p>
+                
+                </p>
+            </VerticalTimelineElement>
+              </VerticalTimeline>
     </div>
     </section>
   )
